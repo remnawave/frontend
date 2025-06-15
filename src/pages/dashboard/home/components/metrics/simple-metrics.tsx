3be676940f8d5546350d707823de2b6f9d5d8796
kastov <kastov@gog.sh>
2025-06-15 19:45:21 +0300

    chore: update @remnawave/backend-contract to version 0.7.23 in package.json and package-lock.json; add 'total-online-on-nodes' metric to localization files

--- src/pages/dashboard/home/components/metrics/simple-metrics.tsx
+++ src/pages/dashboard/home/components/metrics/simple-metrics.tsx
@@ -1,4 +1,4 @@
-import { PiChartBarDuotone, PiClockDuotone, PiMemoryDuotone } from 'react-icons/pi'
+import { PiChartBarDuotone, PiClockDuotone, PiCpuDuotone, PiMemoryDuotone } from 'react-icons/pi'
 import { GetStatsCommand } from '@remnawave/backend-contract'
 import { i18n, TFunction } from 'i18next'
 import dayjs from 'dayjs'
@@ -10,12 +10,18 @@ export const getSimpleMetrics = (
     t: TFunction,
     i18n: i18n
 ) => {
-    const { memory, users } = systemInfo
+    const { memory, users, nodes } = systemInfo
 
     const totalRamGB = prettyBytesUtil(memory.total) ?? 0
     const usedRamGB = prettyBytesUtil(memory.active) ?? 0
 
     return [
+        {
+            value: nodes.totalOnline,
+            icon: PiCpuDuotone,
+            title: t('simple-metrics.total-online-on-nodes'),
+            color: 'var(--mantine-color-blue-4)'
+        },
         {
             value: prettyBytesUtilWithoutPrefix(Number(users.totalTrafficBytes)) ?? 0,
             icon: PiChartBarDuotone,
