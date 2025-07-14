f782758df3bc0695059246dcb93e347efa5ca6c6
kastov <kastov@gog.sh>
2025-07-14 21:45:49 +0300

    refactor: update favicon resolver to use Google favicon service

--- src/shared/utils/misc/favicon-resolver.ts
+++ src/shared/utils/misc/favicon-resolver.ts
@@ -4,7 +4,8 @@ export const faviconResolver = (faviconLink: null | string) => {
     if (!faviconLink) return null
     const hostLink = parseURL(faviconLink).host
     if (hostLink) {
-        return `https://icons.duckduckgo.com/ip2/${hostLink}.ico`
+        // return `https://icons.duckduckgo.com/ip2/${hostLink}.ico`
+        return `https://www.google.com/s2/favicons?sz=64&domain=${hostLink}`
     }
     return null
 }
