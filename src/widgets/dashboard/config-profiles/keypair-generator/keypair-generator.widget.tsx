244fb15ca642b0c8a95d14f81c0f569a05840a36
kastov <kastov@gog.sh>
2025-10-09 17:24:59 +0300

    feat: add ML-KEM768 key generation to KeypairGeneratorWidget and update backend-contract dependency to version 2.1.68

--- src/widgets/dashboard/config-profiles/keypair-generator/keypair-generator.widget.tsx
+++ src/widgets/dashboard/config-profiles/keypair-generator/keypair-generator.widget.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable camelcase */
 import { Button, Divider, Group, px, Stack, Tabs, Transition } from '@mantine/core'
-import { PiGearSixDuotone, PiKey, PiNoteDuotone } from 'react-icons/pi'
+import { TbKey, TbLock, TbSignature } from 'react-icons/tb'
 import { randomBytes } from '@noble/post-quantum/utils.js'
+import { ml_kem768 } from '@noble/post-quantum/ml-kem.js'
 import { ml_dsa65 } from '@noble/post-quantum/ml-dsa.js'
 import { generateKeyPair } from '@stablelib/x25519'
 import { encodeURLSafe } from '@stablelib/base64'
 import { useTranslation } from 'react-i18next'
 import { useEffect, useState } from 'react'
+import { PiKey } from 'react-icons/pi'
 
 import { CopyableFieldShared } from '@shared/ui/copyable-field/copyable-field'
 import { CopyableAreaShared } from '@shared/ui/copyable-area/copyable-area'
@@ -14,14 +16,15 @@ import { CopyableAreaShared } from '@shared/ui/copyable-area/copyable-area'
 import classes from './KeypairGenerator.module.css'
 
 const enum TabTypes {
-    CLASSIC = 'classic',
-    ML_DSA65 = 'ml-dsa65'
+    ML_DSA65 = 'ml-dsa65',
+    ML_KEM768 = 'ml-kem768',
+    X25519 = 'x25519'
 }
 
 export const KeypairGeneratorWidget = () => {
     const { t } = useTranslation()
 
-    const [activeTab, setActiveTab] = useState<TabTypes>(TabTypes.CLASSIC)
+    const [activeTab, setActiveTab] = useState<TabTypes>(TabTypes.X25519)
 
     const [keyPair, setKeyPair] = useState<{
         privateKey: string
@@ -37,6 +40,14 @@ export const KeypairGeneratorWidget = () => {
     }>({
         mldsa65Verify: '',
         mldsa65Seed: ''
+    })
+
+    const [mlKem768KeyPair, setMlKem768KeyPair] = useState<{
+        mlkem768PublicKey: string
+        mlkem768Seed: string
+    }>({
+        mlkem768PublicKey: '',
+        mlkem768Seed: ''
     })
 
     const generatePublicAndPrivate = () => {
@@ -58,9 +69,21 @@ export const KeypairGeneratorWidget = () => {
         })
     }
 
+    const generateMlKem768KeyPair = () => {
+        const seed = randomBytes(64)
+        const mlkem768KeyPair = ml_kem768.keygen(seed)
+        setMlKem768KeyPair({
+            mlkem768PublicKey: encodeURLSafe(mlkem768KeyPair.publicKey)
+                .replace(/=/g, '')
+                .replace(/\n/g, ''),
+            mlkem768Seed: encodeURLSafe(seed).replace(/=/g, '').replace(/\n/g, '')
+        })
+    }
+
     useEffect(() => {
         generatePublicAndPrivate()
         generateMlDsa65KeyPair()
+        generateMlKem768KeyPair()
     }, [])
 
     return (
@@ -74,27 +97,35 @@ export const KeypairGeneratorWidget = () => {
             >
                 <Tabs.List grow mb="md">
                     <Tabs.Tab
-                        key={TabTypes.CLASSIC}
-                        leftSection={<PiNoteDuotone size={16} />}
-                        value={TabTypes.CLASSIC}
+                        key={TabTypes.X25519}
+                        leftSection={<TbKey size={16} />}
+                        value={TabTypes.X25519}
                     >
-                        Classic
+                        X25519
                     </Tabs.Tab>
 
                     <Tabs.Tab
                         key={TabTypes.ML_DSA65}
-                        leftSection={<PiGearSixDuotone size={16} />}
+                        leftSection={<TbSignature size={16} />}
                         value={TabTypes.ML_DSA65}
                     >
                         ML-DSA65
                     </Tabs.Tab>
+
+                    <Tabs.Tab
+                        key={TabTypes.ML_KEM768}
+                        leftSection={<TbLock size={16} />}
+                        value={TabTypes.ML_KEM768}
+                    >
+                        ML-KEM768
+                    </Tabs.Tab>
                 </Tabs.List>
 
-                <Tabs.Panel value={TabTypes.CLASSIC}>
+                <Tabs.Panel value={TabTypes.X25519}>
                     <Transition
                         duration={200}
                         keepMounted
-                        mounted={activeTab === TabTypes.CLASSIC}
+                        mounted={activeTab === TabTypes.X25519}
                         timingFunction="linear"
                         transition="fade"
                     >
@@ -172,6 +203,44 @@ export const KeypairGeneratorWidget = () => {
                                     <Button
                                         leftSection={<PiKey size={px('1.2rem')} />}
                                         onClick={generateMlDsa65KeyPair}
+                                        radius="md"
+                                        size="sm"
+                                        variant="default"
+                                    >
+                                        {t('keypair.widget.generate-key-pair')}
+                                    </Button>
+                                </Group>
+                            </Stack>
+                        )}
+                    </Transition>
+                </Tabs.Panel>
+
+                <Tabs.Panel value={TabTypes.ML_KEM768}>
+                    <Transition
+                        duration={200}
+                        keepMounted
+                        mounted={activeTab === TabTypes.ML_KEM768}
+                        timingFunction="linear"
+                        transition="fade"
+                    >
+                        {(styles) => (
+                            <Stack gap="md" style={styles}>
+                                <Stack gap="xs">
+                                    <CopyableFieldShared
+                                        label="Server side, used in decryption"
+                                        value={mlKem768KeyPair.mlkem768Seed}
+                                    />
+
+                                    <CopyableFieldShared
+                                        label="Client side, used in encryption"
+                                        value={mlKem768KeyPair.mlkem768PublicKey}
+                                    />
+                                </Stack>
+
+                                <Group justify="flex-end">
+                                    <Button
+                                        leftSection={<PiKey size={px('1.2rem')} />}
+                                        onClick={generateMlKem768KeyPair}
                                         radius="md"
                                         size="sm"
                                         variant="default"
