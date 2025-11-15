14a475cba139ea589764d8ccad796fc55b40d771
ingria <codefuhrer@gmail.com>
2025-11-15 12:13:30 +0300

    Update favicon-resolver.ts
    fixes https://github.com/remnawave/panel/issues/219

--- src/shared/utils/misc/favicon-resolver.ts
+++ src/shared/utils/misc/favicon-resolver.ts
@@ -2,10 +2,10 @@ import { parseURL } from 'ufo'
 
 export const faviconResolver = (faviconLink: null | string) => {
     if (!faviconLink) return null
-    const hostLink = parseURL(faviconLink).host
-    if (hostLink) {
-        // return `https://icons.duckduckgo.com/ip2/${hostLink}.ico`
-        return `https://www.google.com/s2/favicons?sz=64&domain=${hostLink}`
+    const { host, protocol } = parseURL(faviconLink, 'https://')
+    if (host) {
+        // return `https://icons.duckduckgo.com/ip2/${host}.ico`
+        return `https://www.google.com/s2/favicons?sz=64&domain_url=${protocol}//${host}`
     }
     return null
 }
