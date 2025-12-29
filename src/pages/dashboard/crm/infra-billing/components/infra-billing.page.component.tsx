32d17e08a91fd1008912c7157abf4ba9765f4600
kastov <kastov@gog.sh>
2025-12-29 18:06:56 +0300

    refactor: remove LandscapeBannerShared component and adjust InfraBillingPageComponent layout

--- src/pages/dashboard/crm/infra-billing/components/infra-billing.page.component.tsx
+++ src/pages/dashboard/crm/infra-billing/components/infra-billing.page.component.tsx
@@ -1,7 +1,6 @@
 import { Split } from '@gfazioli/mantine-split-pane'
-import { useMediaQuery } from '@mantine/hooks'
 import { useTranslation } from 'react-i18next'
-import { em, Stack } from '@mantine/core'
+import { Stack } from '@mantine/core'
 import { motion } from 'motion/react'
 
 import { CreateInfraBillingRecordDrawerWidget } from '@widgets/dashboard/infra-billing/create-infra-billing-record-modal/create-infra-billing-record.modal.widget'
@@ -12,13 +11,11 @@ import { ViewInfraProviderDrawerWidget } from '@widgets/dashboard/infra-billing/
 import { InfraBillingNodesTableWidget } from '@widgets/dashboard/infra-billing/infra-billing-nodes-table/infra-billing-nodes.widget'
 import { InfraProvidersTableWidget } from '@widgets/dashboard/infra-billing/infra-providers-table/infra-providers-table.widget'
 import { UpdateBillingDateModalWidget } from '@widgets/dashboard/infra-billing/update-billing-date-modal'
-import { LandscapeBannerShared } from '@shared/ui/landscape-banner/landscape-banner.shared'
 import { StatsWidget } from '@widgets/dashboard/infra-billing/stats-widget/stats.widget'
 import { Page } from '@shared/ui/page'
 
 export const InfraBillingPageComponent = () => {
     const { t } = useTranslation()
-    const isMobile = useMediaQuery(`(max-width: ${em(450)})`)
     return (
         <Page title={t('constants.infra-billing')}>
             <motion.div
@@ -27,25 +24,22 @@ export const InfraBillingPageComponent = () => {
                 transition={{ duration: 0.5 }}
             >
                 <StatsWidget />
-                {isMobile ? (
-                    <LandscapeBannerShared />
-                ) : (
-                    <Stack>
-                        <Split spacing="sm" variant="dotted">
-                            <Split.Pane initialWidth="60%">
-                                <InfraBillingNodesTableWidget />
-                            </Split.Pane>
 
-                            <Split.Resizer />
+                <Stack>
+                    <Split spacing="sm" variant="dotted">
+                        <Split.Pane initialWidth="60%">
+                            <InfraBillingNodesTableWidget />
+                        </Split.Pane>
 
-                            <Split.Pane grow initialWidth="40%">
-                                <InfraBillingRecordsTableWidget />
-                            </Split.Pane>
-                        </Split>
+                        <Split.Resizer />
 
-                        <InfraProvidersTableWidget />
-                    </Stack>
-                )}
+                        <Split.Pane grow initialWidth="40%">
+                            <InfraBillingRecordsTableWidget />
+                        </Split.Pane>
+                    </Split>
+
+                    <InfraProvidersTableWidget />
+                </Stack>
             </motion.div>
 
             <ViewInfraProviderDrawerWidget />
