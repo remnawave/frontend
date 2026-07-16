23314c3727ec599af0ebb181154bfa2d9cd63d19
kastov <yk@sent.com>
2026-07-17 00:41:17 +0300

    refactor: simplify ConfigProfileInboundsDrawer

--- src/shared/_modals/config-profiles/config-profile-inbounds-drawer/config-profile-inbounds.drawer.widget.tsx
+++ src/shared/_modals/config-profiles/config-profile-inbounds-drawer/config-profile-inbounds.drawer.widget.tsx
@@ -1,29 +1,13 @@
 import NiceModal, { useModal } from '@ebay/nice-modal-react'
-import {
-    ActionIcon,
-    Badge,
-    Box,
-    Card,
-    Center,
-    CopyButton,
-    Drawer,
-    Group,
-    Loader,
-    Stack,
-    Text,
-    Tooltip
-} from '@mantine/core'
-import { modals } from '@mantine/modals'
-import ColorHash from 'color-hash'
-import { githubDarkTheme, JsonEditor } from 'json-edit-react'
+import { Center, Drawer, Loader, Stack, Text } from '@mantine/core'
 import { useTranslation } from 'react-i18next'
-import { PiCheck, PiCopy } from 'react-icons/pi'
-import { TbCirclesRelation, TbTag } from 'react-icons/tb'
+import { TbCirclesRelation } from 'react-icons/tb'
 
 import { useNiceMantineModal } from '@shared/_modals/use-nice-modal'
 import { useGetConfigProfileInbounds, useGetInternalSquads } from '@shared/api/hooks'
-import { InternalSquadsListSimpleWidgetShared } from '@shared/ui/internal-squads/internal-squads-list-simple'
 import { BaseOverlayHeader } from '@shared/ui/overlays/base-overlay-header'
+
+import { ConfigProfileInboundsTree } from './config-profile-inbounds.tree'
 
 interface IProps {
     uuid: string
@@ -58,125 +42,6 @@ export const ConfigProfileInboundsDrawer = NiceModal.create((props: IProps) => {
         )
     }
 
-    const renderInbounds = () => {
-        if (!configProfileInbounds) return null
-        if (!internalSquads) return null
-
-        const colorHash = new ColorHash({ lightness: 0.7, saturation: 0.6 })
-
-        return (
-            <Stack gap="xl">
-                <Stack gap="lg">
-                    {configProfileInbounds.inbounds.map((inbound) => {
-                        const filteredSquads = internalSquads.internalSquads.filter((squad) =>
-                            inbound.activeSquads.some((squadUuid) => squadUuid === squad.uuid)
-                        )
-                        const tagColor = colorHash.hex(inbound.tag)
-
-                        return (
-                            <Card
-                                bg="dark.6"
-                                key={inbound.uuid}
-                                p="xs"
-                                style={{
-                                    borderLeft: `4px solid ${tagColor}`
-                                }}
-                                withBorder
-                            >
-                                <Stack gap="sm">
-                                    <Group align="center" justify="space-between" wrap="nowrap">
-                                        <Group align="center" gap="sm" wrap="nowrap">
-                                            <ActionIcon
-                                                color={tagColor}
-                                                onClick={(e) => {
-                                                    e.stopPropagation()
-                                                    modals.open({
-                                                        children: (
-                                                            <Box>
-                                                                <JsonEditor
-                                                                    collapse={3}
-                                                                    data={
-                                                                        inbound.rawInbound as object
-                                                                    }
-                                                                    indent={4}
-                                                                    maxWidth="100%"
-                                                                    rootName=""
-                                                                    theme={githubDarkTheme}
-                                                                    viewOnly
-                                                                />
-                                                            </Box>
-                                                        ),
-                                                        title: (
-                                                            <BaseOverlayHeader
-                                                                iconColor="teal"
-                                                                IconComponent={TbTag}
-                                                                iconVariant="soft"
-                                                                title={inbound.tag}
-                                                                titleOrder={5}
-                                                            />
-                                                        ),
-                                                        size: 'xl'
-                                                    })
-                                                }}
-                                                size="md"
-                                                variant="filled"
-                                            >
-                                                <TbTag
-                                                    color="var(--mantine-color-dark-8)"
-                                                    size={20}
-                                                />
-                                            </ActionIcon>
-
-                                            <Text fw={600} size="md" truncate>
-                                                {inbound.tag}
-                                            </Text>
-                                        </Group>
-
-                                        <Group gap={0} mr="md" wrap="nowrap">
-                                            <Badge
-                                                leftSection={<TbCirclesRelation size="18" />}
-                                                size="lg"
-                                                variant="transparent"
-                                            >
-                                                {filteredSquads.length}
-                                            </Badge>
-                                            <CopyButton timeout={2000} value={inbound.uuid}>
-                                                {({ copied, copy }) => (
-                                                    <Tooltip
-                                                        label={copied ? 'Copied!' : 'Copy UUID'}
-                                                    >
-                                                        <ActionIcon
-                                                            color={copied ? 'teal' : 'gray'}
-                                                            onClick={copy}
-                                                            size="sm"
-                                                            variant="subtle"
-                                                        >
-                                                            {copied ? (
-                                                                <PiCheck size={18} />
-                                                            ) : (
-                                                                <PiCopy size={18} />
-                                                            )}
-                                                        </ActionIcon>
-                                                    </Tooltip>
-                                                )}
-                                            </CopyButton>
-                                        </Group>
-                                    </Group>
-
-                                    {filteredSquads.length > 0 && (
-                                        <InternalSquadsListSimpleWidgetShared
-                                            filteredInternalSquads={filteredSquads}
-                                        />
-                                    )}
-                                </Stack>
-                            </Card>
-                        )
-                    })}
-                </Stack>
-            </Stack>
-        )
-    }
-
     return (
         <Drawer
             {...modalProps}
@@ -193,7 +58,12 @@ export const ConfigProfileInboundsDrawer = NiceModal.create((props: IProps) => {
             }
         >
             {(isLoading || isLoadingInternalSquads) && returnLoading()}
-            {!isLoading && !isLoadingInternalSquads && configProfileInbounds && renderInbounds()}
+            {!isLoading && !isLoadingInternalSquads && configProfileInbounds && internalSquads && (
+                <ConfigProfileInboundsTree
+                    inbounds={configProfileInbounds.inbounds}
+                    internalSquads={internalSquads.internalSquads}
+                />
+            )}
         </Drawer>
     )
 })
