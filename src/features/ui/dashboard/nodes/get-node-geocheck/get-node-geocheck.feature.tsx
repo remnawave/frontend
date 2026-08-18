f3d15814a0552f9f833405f3aedeccb87ddb0dcf
kastov <yk@sent.com>
2026-08-19 01:50:42 +0300

    feat: add node version requirement check in Geocheck feature

--- src/features/ui/dashboard/nodes/get-node-geocheck/get-node-geocheck.feature.tsx
+++ src/features/ui/dashboard/nodes/get-node-geocheck/get-node-geocheck.feature.tsx
@@ -1,10 +1,13 @@
 import { ActionIcon, Tooltip } from '@mantine/core'
 import { GetNodeCommand } from '@remnawave/backend-contract'
-import { memo } from 'react'
+import { memo, useMemo } from 'react'
 import { useTranslation } from 'react-i18next'
 import { TbMapSearch } from 'react-icons/tb'
+import semver from 'semver'
 
 import { showModal } from '@shared/_modals/show-modal'
+
+const MIN_NODE_VERSION = '3.3.0'
 
 interface IProps {
     node: GetNodeCommand.Response['response']
@@ -14,11 +17,28 @@ const GetNodeGeocheckFeatureComponent = (props: IProps) => {
     const { node } = props
     const { t } = useTranslation()
 
+    const nodeVersion = node.versions?.node
+
+    const isSupported = useMemo(() => {
+        const version = semver.coerce(nodeVersion)
+
+        return version !== null && semver.gte(version, MIN_NODE_VERSION)
+    }, [nodeVersion])
+
     return (
-        <Tooltip label={t('node-geocheck.title')}>
+        <Tooltip
+            label={
+                isSupported
+                    ? t('node-geocheck.title')
+                    : t('node-geocheck.requires-node-version', { version: MIN_NODE_VERSION })
+            }
+        >
             <ActionIcon
+                disabled={!isSupported}
                 color="indigo"
-                onClick={() => showModal('nodes_nodeGeocheckModal', { node })}
+                onClick={() => {
+                    showModal('nodes_nodeGeocheckModal', { node })
+                }}
                 size="lg"
                 variant="soft"
             >
